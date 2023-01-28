217d45a1becd5bc2bf1d5f8afdc9143808078412
pedro-morachacon <122901168+pedro-morachacon@users.noreply.github.com>
2023-01-28 17:36:14 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sys_info_comp30380_pamc',
+      version='0.1',
+      description='Project to find system information',
+      url='https://github.com/pedro-morachacon/systeminfo',
+      author='pedroAntonioMoraChacon',
+      author_email='pedro.morachacon@ucdconnect.ie',
+      license='',
+      packages=['sys_info_comp30380_pamc'],
+      zip_safe=False)
